5d1ef8f54739e1387af8bc0ed38996c91b3d04bc
Chris Birdsong <34635830+smtpman@users.noreply.github.com>
2022-06-30 19:48:38 -0500

    Update install.zsh

--- install.zsh
+++ install.zsh
@@ -1,1 +1,13 @@
 #!/bin/zsh
+
+
+brew install git
+Echo "Installing GIT"
+brew install --cask google-chrome
+Echo "Installing Google Chrome"
+brew install --cask raspberry-pi-imager
+Echo "Installing Raspberry-PI-Imager"
+brew install nmap
+Echo "Installing NMAP"
+brew install netcat
+Echo "Installing NETCAT"
